af59435dabf05e8c1136e960023c70e086320f1a
hickee032 <hickee32@gmail.com>
2023-10-21 12:00:00 +0900

    Note_Pad

---
+++ NotePad/Classes/MainTabItem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace NotePad.Classes
+{
+    public class MainTabItem : PropertyChangedStuff
+    {
+
+        public List<TabItem> Item { get; set; }
+
+        public List<string> FilePaths { get; set; }
+
+        public List<string> FileData { get; set; }
+
+        private int _selectedItem;
+
+        public int SelectedItem {
+
+            get { return _selectedItem; }
+            set { _selectedItem = value; RaisePropertyChanged(nameof(SelectedItem)); }
+        }
+
+        public MainTabItem() {
+            Item = new List<TabItem>();
+            FilePaths = new List<string>();
+            FileData = new List<string>();
+        }
+
+        #region variable
+        private string _defaultName = "New Item";
+        #endregion
+
+        public void AddNewTabItem() {
+            TabItem tab = new TabItem();
+            TextBox tb = new TextBox();
+        }
+    }
+}
