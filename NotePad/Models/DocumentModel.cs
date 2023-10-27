cd46bc87a3ff0fa1d68d5de258816ad0f925ad08
hickee032 <hickee32@gmail.com>
2023-10-27 12:00:00 +0900

    Note_Pad

--- NotePad/Models/DocumentModel.cs
+++ NotePad/Models/DocumentModel.cs
@@ -6,7 +6,40 @@ using System.Threading.Tasks;
 
 namespace NotePad.Models
 {
-    public class DocumentModel
+    public class DocumentModel : ObservableObject
     {
+        private string _text;
+        public string Text {
+            get { return _text; }
+            set { OnpropertyChanged(ref _text, value); }
+        
+        }
+
+        private string _filePath;
+        public string FilePath {
+            get { return _filePath; }
+            set { OnpropertyChanged(ref _filePath, value); }
+
+        }
+
+        private string _fileName;
+        public string FileName {
+            get { return _fileName; }
+            set { OnpropertyChanged(ref _fileName, value); }
+
+        }
+
+        public bool isEmpty {
+
+            get {
+                if(string.IsNullOrEmpty(FileName)||
+                    string.IsNullOrEmpty(FilePath)) {
+                    return true;
+                }
+                return false;
+            }
+        }
+
+
     }
 }
