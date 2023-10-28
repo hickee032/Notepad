f822835049e22e71a91faae7a89b54d2eac61133
hickee032 <hickee32@gmail.com>
2023-10-28 12:00:00 +0900

    Note_Pad

--- NotePad/ViewModels/FileViewModel.cs
+++ NotePad/ViewModels/FileViewModel.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using NotePad.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace NotePad.ViewModels
 {
     public class FileViewModel
     {
+        public DocumentModel Document { get; private set; }
+
+        public ICommand NewCommand { get;}
+        public ICommand SaveCommand { get; }
+        public ICommand SaveAsCommand { get; }
+        public ICommand OpenCommand { get; }
+
     }
 }
