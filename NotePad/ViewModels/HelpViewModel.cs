9078b1e472684f001155c30247405f747fa93c14
hickee032 <hickee32@gmail.com>
2023-11-04 12:00:00 +0900

    Note_Pad

--- NotePad/ViewModels/HelpViewModel.cs
+++ NotePad/ViewModels/HelpViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Notepad;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,6 +19,7 @@ namespace NotePad.ViewModels
 
             var helpDialog = new HelpDialog();
             helpDialog.ShowDialog();
+            
         }
     }
 }
